bffa157cf6c00df714c68ddaecde4b76449ed35d
Caio Ghinis <caio.ghinis1@gmail.com>
2023-01-13 14:12:13 -0300

    Project created

---
+++ SalesModel/SalesModel/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SalesModel.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
